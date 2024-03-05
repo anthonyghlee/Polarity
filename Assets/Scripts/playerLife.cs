f87d4afb2934ade042526040c1395468649ea1f2
Kevin Tran <kevintran9250@gmail.com>
2024-03-05 10:55:39 -0800

    Added Life, moving, and sticky platforms

---
+++ Assets/Scripts/playerLife.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerLife : MonoBehaviour
+{
+    // private Animator animator;
+    private Rigidbody2D rb;
+
+    // [SerializeField] private AudioSource deathSoundEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            // deathSoundEffect.Play();
+            Die();
+
+        }
+    }
+
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        RestartLevel();
+        // animator.SetTrigger("death");
+
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/stickyPlatform.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickyPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (collision.gameObject.name == "Player")
+        {
+            playerRb.gravityScale = 0f;
+
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (collision.gameObject.name == "Player")
+        {
+            playerRb.gravityScale = 2f;
+
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
