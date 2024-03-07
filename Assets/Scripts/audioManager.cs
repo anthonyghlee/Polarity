0cdd06b88b9298f6ae02166b88d3a83534f02920
Kevin Tran <kevintran9250@gmail.com>
2024-03-06 21:27:15 -0800

    added some progress on sounds

---
+++ Assets/Scripts/audioManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(sound s in sounds)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
